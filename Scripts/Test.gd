c8268b631b8fa19a7f3b3aebd1154c99f3e74a73
Giuseppe Scarlato <giuseppe499@live.it>
2022-04-13 23:57:16 +0200

    Aggiunto script test

---
+++ Scripts/Test.gd
@@ -0,0 +1,21 @@
+extends Sprite
+
+
+export var vel = 100
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("ui_up"):
+		position.y -= vel
+	if Input.is_action_pressed("ui_down"):
+		position.y += vel
+	if Input.is_action_pressed("ui_right"):
+		position.x += vel
+	if Input.is_action_pressed("ui_left"):
+		position.x -= vel
